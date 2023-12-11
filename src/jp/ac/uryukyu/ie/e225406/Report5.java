72d2fa2927a6caabfa357c6770d5715f331e6f2a
Tairin Nasu <e225406@ie.u-ryukyu.ac.jp>
2023-12-11 20:46:08 +0900

    1stcommit

---
+++ src/jp/ac/uryukyu/ie/e225406/Report5.java
@@ -0,0 +1,11 @@
+package jp.ac.uryukyu.ie.e225406;
+
+public class Report5 {
+    public static void main(String[] args) {
+        // System.out.println("Hello, World!");
+
+        String str = null;
+
+        System.out.println(str.length());
+    }    
+}
