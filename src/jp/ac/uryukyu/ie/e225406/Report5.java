f4fc6b0134c7c6b88cfcaba8d40dde74095cd626
Tairin Nasu <e225406@ie.u-ryukyu.ac.jp>
2023-12-11 21:54:53 +0900

    3thcommit

--- src/jp/ac/uryukyu/ie/e225406/Report5.java
+++ src/jp/ac/uryukyu/ie/e225406/Report5.java
@@ -2,17 +2,13 @@ package jp.ac.uryukyu.ie.e225406;
 
 public class Report5 {
     public static void main(String[] args) {
-        // System.out.println("Hello, World!");
 
-        String str = null;
+        String str = "壱百満";
 
-        // System.out.println(str.length());
-
-        try {
-            System.out.println("String length: " + str.length());
-        } catch (NullPointerException e) {
-            // 実行時エラー（NullPointerException）が発生した場合の処理
-            System.err.println("NullPointerException caught: " + e.getMessage());
+        try{
+            int value = Integer.parseInt(str);
+        } catch(Exception e){
+            System.out.println("代入できませんでした");
         }
     }    
 }
